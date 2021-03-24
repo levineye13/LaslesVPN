eb671b0d2621d04da2af7c32b9905d835be90e3a
Oleg Zharov <levineye@gmail.com>
2021-03-24 14:11:53 +0300

    feat: выводить шрифты и изображения в отдельные папки

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/pages/index.scss',
+  entry: {
+    main: './src/pages/index.scss',
+    fonts: './src/vendor/fonts/fonts.scss',
+  },
   output: {
     path: resolve(__dirname, 'dist'),
   },
   devServer: {
     contentBase: resolve(__dirname, 'dist'),
     compress: true,
+    host: 'localhost',
     port: 8080,
     open: true,
+    writeToDisk: true,
+    watchContentBase: true,
   },
   module: {
     rules: [
@@ -23,15 +30,22 @@ module.exports = {
       {
         test: /\.(png|jpg|svg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext]',
+        },
       },
       {
         test: /\.(eot|ttf|woff(2)?)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext]',
+        },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
   ],
 };
