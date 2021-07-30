feeefc20691494e4200a455690900ea871c7afde
Oleg Zharov <levineye@gmail.com>
2021-07-30 16:51:31 +0300

    feat: написать и подключить валидатор формы

---
+++ src/components/FormValidator.js
@@ -0,0 +1,52 @@
+class FormValidator {
+  constructor(elementWithForm) {
+    this._htmlElementWithForm = elementWithForm;
+    this._form = this._htmlElementWithForm.querySelector('form');
+  }
+
+  _setSubmitState = (isFormValid, submitElement) => {
+    if (isFormValid) {
+      return submitElement.removeAttribute('disabled');
+    }
+    return submitElement.setAttribute('disabled', true);
+  };
+
+  _checkInputValidity = (inputList) => {
+    return inputList.some((input) => {
+      console.log(input.validity);
+      return !input.validity.valid;
+    });
+  };
+
+  _checkFormValidity = (inputElements, submitElement) => {
+    const isValidInputs = !this._checkInputValidity(inputElements);
+    console.log(isValidInputs);
+    this._setSubmitState(isValidInputs, submitElement);
+  };
+
+  _setInputListeners = (inputList, submitElement) => {
+    inputList.forEach((input) => {
+      input.addEventListener('input', () =>
+        this._checkFormValidity(inputList, submitElement)
+      );
+    });
+  };
+
+  validate = () => {
+    const formElements = Array.from(this._form.elements);
+
+    const inputElements = formElements.filter(
+      (element) => element.nodeName === 'INPUT'
+    );
+
+    const submitElement = formElements.find((element) => {
+      return element.nodeName === 'BUTTON' && element.type === 'submit';
+    });
+
+    this._setInputListeners(inputElements, submitElement);
+
+    this._checkFormValidity(inputElements, submitElement);
+  };
+}
+
+export default FormValidator;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import './index.scss';
 import Popup from './components/Popup';
+import FormValidator from './components/FormValidator';
 
 const buttonOpenLoginPopup = document.querySelector('.header__signin');
 const buttonOpenRegisterPopup = document.querySelector('.header__signup');
 
 const loginPopup = new Popup('.popup_type_login');
 const registerPopup = new Popup('.popup_type_register');
+
+const loginWithValidation = new FormValidator(loginPopup.popupHtmlElement);
+loginWithValidation.validate();
+
+const registerWithValidation = new FormValidator(
+  registerPopup.popupHtmlElement
+);
+registerWithValidation.validate();
 
 buttonOpenLoginPopup.addEventListener('click', loginPopup.open);
 buttonOpenRegisterPopup.addEventListener('click', registerPopup.open);
