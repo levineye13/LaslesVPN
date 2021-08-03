992d7cf8b20ba67812d89be7b58d06d4e461f530
Oleg Zharov <levineye@gmail.com>
2021-08-03 19:15:56 +0300

    feat: верстануть сайдбар

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import FormValidator from './components/FormValidator';
 
 const buttonOpenLoginPopup = document.querySelector('.header__signin');
 const buttonOpenRegisterPopup = document.querySelector('.header__signup');
+const buttonMenu = document.querySelector('.header__button-menu');
+const sidebar = document.querySelector('.sidebar');
 
 const loginPopup = new Popup('.popup_type_login');
 const registerPopup = new Popup('.popup_type_register');
@@ -18,3 +20,6 @@ registerWithValidation.validate();
 
 buttonOpenLoginPopup.addEventListener('click', loginPopup.open);
 buttonOpenRegisterPopup.addEventListener('click', registerPopup.open);
+buttonMenu.addEventListener('click', () =>
+  sidebar.classList.add('sidebar_opened')
+);
