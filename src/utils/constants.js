38c79c34c636b2ba28c5377d286648eb30c38902
levineye <levineye@gmail.com>
2021-08-05 21:42:28 +0300

    feat: вынести константы в соответствующий файл

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,17 @@ export const HTML_NODES = {
   input: 'INPUT',
   button: 'BUTTON',
 };
+
+export const selectors = [
+  '.header__button-menu',
+  '.header__signin',
+  '.header__signup',
+  '.sidebar__signin',
+  '.sidebar__signup',
+];
+
+export const modalSelectors = [
+  '.popup_type_login',
+  '.popup_type_register',
+  '.sidebar',
+];
